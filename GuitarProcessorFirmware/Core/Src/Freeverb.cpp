3d1167ea4d9beab6a64aae52752390e2d7535c46
Hayden Setlik <haydensetlik@gmail.com>
2026-04-30 20:01:38 -0600

    board too tall ughhhhh

--- GuitarProcessorFirmware/Core/Src/Freeverb.cpp
+++ GuitarProcessorFirmware/Core/Src/Freeverb.cpp
@@ -5,16 +5,15 @@ void LBCF::setBuffer(float* buffAdr, uint32_t size){
     bufSize = size;
     head = 0;
     for(uint32_t i = 0; i < bufSize; ++i){
-        buffer[i] = 0;
+        buffer[i] = 0.0f;
     }
 }
-
 
 void FVAllpass::setBuffer(float* buffAdr, uint32_t size){
     buffer = buffAdr;
     bufSize = size;
     head = 0;
     for(uint32_t i = 0; i < bufSize; ++i){
-        buffer[i] = 0;
+        buffer[i] = 0.0f;
     }
 }
